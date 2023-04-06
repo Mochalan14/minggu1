e6954f0d87e5862cd7c4ffe6c09c958bc44abbee
Mochammad Maulana <mumetofficial@gmail.com>
2023-04-06 15:29:52 +0700

    update fitur upload file

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -53,9 +54,37 @@ class PostController extends Controller
     public function update(Request $request,$id){
         $post = Post::find($id);
 
-        $post->Judul = $request->judul;
-        $post->Deskripsi = $request->deskripsi;
-        $post->save();
+        if ($request->hasFile('photo')) {
+
+            $file = $request->file('photo');
+            $filename = time() . '.' .$file->getClientOriginalExtension();
+    
+            $photo_path = $request->file('photo')->storeAs('public/posts',$filename);
+            $photo_path = str_replace('public/','',$photo_path);
+    
+
+            // //delete old image
+            Storage::delete('public/'.$post->photo);
+
+            //update post with new image
+            $post->update([
+                'Judul' => $request->judul,
+                'Deskripsi' => $request->judul,
+                'photo' => $photo_path
+            ]);
+
+        } else {
+
+            //update post without image
+            $post->update([
+                'Judul'     => $request->judul,
+                'Deskripsi'   => $request->deskripsi
+            ]);
+        }
+
+        // $post->Judul = $request->judul;
+        // $post->Deskripsi = $request->deskripsi;
+        // $post->save();
 
         return redirect()->route('post.index');
     }
